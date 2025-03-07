6dab7eb650714f4d4855ffca7ef3ae6cff45345f
Vinícius de Oliveira Souza <viniciuso970@gmail.com>
2025-03-07 10:42:16 -0300

    Starting the vos_base theme.

---
+++ web/themes/custom/vos_base/src/js/_toast-init.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-unresolved, no-restricted-syntax */
+import Toast from 'bootstrap/js/dist/toast';
+
+// * Run toasts on page load
+(() => {
+  document.addEventListener('DOMContentLoaded', () => {
+    const toastElList = [...document.querySelectorAll('.toast')];
+    for (const toastEl of toastElList) {
+      const toast = new Toast(toastEl, { autohide: false });
+      toast.show();
+    }
+  });
+})();
+
+// * Run toasts on button trigger example
+// (() => {
+//   document.addEventListener("DOMContentLoaded", () => {
+//     const toastTrigger = document.getElementById('liveToastBtn');
+//     const toastLiveExample = document.getElementById('liveToast');
+
+//     if (toastTrigger && toastLiveExample) {
+//       const toastBootstrap = Toast.getOrCreateInstance(toastLiveExample);
+//       toastTrigger.addEventListener('click', () => {
+//         toastBootstrap.show();
+//       });
+//     }
+//   });
+// })();
